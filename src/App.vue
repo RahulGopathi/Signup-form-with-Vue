8fa89fa717d33f49383948c75f37c1f647deee42
Rahul Gopathi <75736709+RahulGopathi@users.noreply.github.com>
2021-06-28 09:42:32 +0530

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Signup />
+</template>
+
+<script>
+import Signup from "./components/Signup_form";
+
+export default {
+  name: "App",
+  components: { Signup },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  margin: 0px;
+  background: #eee;
+}
+</style>
